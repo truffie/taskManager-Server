f2e834c02ca0c35b262a0963cb46d4fdb1f78c6a
Truffie <trufanov.vl2013@gmail.com>
2024-01-10 22:13:05 +0300

    feat:add user body validation

--- src/helper/validation.js
+++ src/helper/validation.js
@@ -10,5 +10,24 @@ function isValidId(req, _res, next) {
   next();
 }
 
+function checkerBody(body) {
+  if ('name' in body) {
+    if (!/^[a-zA-Z ]{2,}$/gm.test(body.name)) throw new Error('invalid name');
+    if (/[ ]{2,}/gm.test(body.name)) throw new Error('multiply spaces');
+    if (body.name.split(' ').length > 3) throw new Error('max 3 parts of name');
+  }
 
-module.exports = { isValidId };
+  if ('surname' in body) {
+    if (!/^[a-zA-Z]{2,}$/gm.test(body.surname)) throw new Error('invalid surname');
+    if (body.surname.includes(' ')) throw new Error('The surname contains a space');
+  }
+  if ('email' in body) {
+    if (!/^[\w]+\.[\w]+@[\w]{4,8}\.\w{2,4}$/gm.test(body.email)) throw new Error('invalid email');
+  }
+}
+
+function isValidUserBody(req, _res, next) {
+  checkerBody(req.body);
+  next();
+}
+module.exports = { isValidId, isValidUserBody };
