49ec158948db39272f6e6963418ad85f2dcda423
Truffie <trufanov.vl2013@gmail.com>
2024-01-11 22:24:37 +0300

    feat:add task controller

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -1,4 +1,61 @@
 const route = require('express').Router();
+const { buildResponse } = require('../helper/buildResponse');
+const { getAllTasks, createTask, getTaskById, updateTask, deleteTask, patchTask } = require('../service/task.service');
+route.get('/', async (_req, res) => {
+  try {
 
+    const data = await getAllTasks();
+    buildResponse(res, 200, data);
+  } catch (error) {
+    buildResponse(res, 404, error.message);
+  }
+});
+route.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await getTaskById(id);
+    buildResponse(res, 200, data);
+  } catch (error) {
+    buildResponse(res, 404, error.message);
+  }
 
+});
+route.post('/', async (req, res) => {
+  try {
+    const { task, user_id } = req.body;
+    const data = await createTask(task, user_id);
+    buildResponse(res, 200, data);
+  } catch (error) {
+    buildResponse(res, 404, error.message);
+  }
+});
+route.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { task, user_id } = req.body;
+    const data = await updateTask(id, task, user_id);
+    buildResponse(res, 200, data);
+  } catch (error) {
+    buildResponse(res, 200, error.message);
+  }
+});
+route.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await deleteTask(id);
+    buildResponse(res, 200, data);
+  } catch (error) {
+    buildResponse(res, 404, error.message);
+  }
+});
+route.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const data = await patchTask(id, body);
+    buildResponse(res, 200, data);
+  } catch (error) {
+    buildResponse(res, 404, error.message);
+  }
+});
 module.exports = route;
