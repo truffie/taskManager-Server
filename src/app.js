252e4b688947250af2711f67717e3ab8e09214be
Truffie <trufanov.vl2013@gmail.com>
2024-01-23 20:58:06 +0300

    feat: add middlewares

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ const app = require('express')();
 const bp = require('body-parser');
 const routeUser = require('./controller/user.controller');
 const routeTask = require('./controller/task.controller');
-
+const routeApi = require('./controller/api.controller');
 
 app.use(bp.json());
 
 app.use('/user', routeUser);
 app.use('/tasks', routeTask);
-
+app.use('/api', routeApi);
 
 app.use((error, _req, res, _next) => {
   res.status(500).send(error.message);
